bda01ff37f758476d1162d214fd1cb35032ffd2c
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2025-05-05 01:29:42 +0200

    Explain why BlockEntityInit is actually necessary.

--- src/main/java/net/teamterminus/machineessentials/util/BlockEntityInit.java
+++ src/main/java/net/teamterminus/machineessentials/util/BlockEntityInit.java
@@ -2,7 +2,8 @@ package net.teamterminus.machineessentials.util;
 
 import net.minecraft.block.Block;
 
-// i don't want to do block entity in a block class method, sorry calm
+// i don't want to do block entity code in a block class method, sorry calm
+// tile entity constructors don't have access to world or position data, and that is why this interface is necessary
 public interface BlockEntityInit {
     void init(Block block);
 }
